b99b3337679b7bb7a4b0c794910d678c108df78f
Kyaw Aung Win <kyawagwin@gmail.com>
2017-04-20 21:45:02 +0800

    initial setup

---
+++ client/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <h3 className="ui center aligned header">Hello World!</h3>
+        );
+    }
+}
